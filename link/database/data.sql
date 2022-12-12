e65d93f5ba64ec6e5714a142cb58cc722abcaac6
yunzeshi <yunze.shi@efrei.net>
2022-12-12 18:40:42 +0100

    sumbit

---
+++ link/database/data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE public.departments
+(
+ id      SERIAL      PRIMARY KEY,
+ name    VARCHAR(20) NOT NULL
+);
+CREATE TABLE public.students
+(
+ id              SERIAL      PRIMARY KEY,
+ department_id   INT         NOT NULL REFERENCES departments (id),
+ first_name      VARCHAR(20) NOT NULL,
+ last_name       VARCHAR(20) NOT NULL
+);
+INSERT INTO departments (name) VALUES ('IRC');
+INSERT INTO departments (name) VALUES ('ETI');
+INSERT INTO departments (name) VALUES ('CGP');
+INSERT INTO students (department_id, first_name, last_name) VALUES (1, 'Eli', 'Copter');
+INSERT INTO students (department_id, first_name, last_name) VALUES (2, 'Emma', 'Carena');
+INSERT INTO students (department_id, first_name, last_name) VALUES (2, 'Jack', 'Uzzi');
+INSERT INTO students (department_id, first_name, last_name) VALUES (3, 'Aude', 'Javel');
